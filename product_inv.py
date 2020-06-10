857773d8f9323de26942665a4a5e0136513bd4dc
jonr13 <65420529+jonr13@users.noreply.github.com>
2020-06-10 18:44:00 -0400

    Update product_inv.py

--- product_inv.py
+++ product_inv.py
@@ -1,1 +1,8 @@
 
+GET /products
+
+curl https://127.0.0.1:5000/products
+curl https://groceries-api-limited.herokuapp.com/products
+curl https://groceries-api-csv.herokuapp.com/products
+
+GET /products/:id
